5ca49c262012568dff55d08c6c01b2957319b2dd
Slimelvl1 <pedroh.af@hotmail.com>
2021-05-17 16:35:25 -0300

    adicionado os arquivos do makemigrations
    close #42

--- pypro/base/migrations/0001_initial.py
+++ pypro/base/migrations/0001_initial.py
@@ -5,6 +5,10 @@ import django.utils.timezone
 import pypro.base.models
 
 
+class Flake8Exception(Exception):
+    """Plain Flake8 exception."""
+
+
 class Migration(migrations.Migration):
 
     initial = True
