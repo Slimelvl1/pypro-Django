714e37fa44564366bceab69ae3e819d9d9e22070
Slimelvl1 <pedroh.af@hotmail.com>
2021-06-22 16:00:25 -0300

    Vídeo de acordo com o slug
    close #81

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+videos = {
+    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação'},
+    {'slug': 'instalacao-windows', 'titulo': 'Video Aperitivo: Motivação'},
+
+}
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação'}
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
